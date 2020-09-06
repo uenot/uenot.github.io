aa0468922a147e0d8393189ccbb5b61969a5d3aa
Toby Ueno <uenot@go.oes.edu>
2020-09-05 18:32:52 -0700

    Updates

---
+++ precache-manifest.2abda0c4b3732425a74ad076709f32e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84151ce0f17070295d1a5a4129d95b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768215eef8f5622efa00",
+    "url": "/static/css/main.b2d33a16.chunk.css"
+  },
+  {
+    "revision": "8920ee0435a19fe06ea5",
+    "url": "/static/js/2.c57ac93c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c57ac93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768215eef8f5622efa00",
+    "url": "/static/js/main.24cd9167.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  }
+]);
